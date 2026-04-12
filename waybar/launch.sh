e29c2a5296662cb0bf5c2bc3e0dc0aa975586827
TecS1 <tec1sierra@gmail.com>
2026-04-12 18:03:37 +0200

    Git initialized, first commit

---
+++ waybar/launch.sh
@@ -0,0 +1,8 @@
+
+#-----------------------------------------------
+
+killall waybar
+
+#-----------------------------------------------
+
+waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
