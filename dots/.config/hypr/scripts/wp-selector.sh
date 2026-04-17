7bd2b13ec1897c3972be669813bca35ba7354872
TecS1 <tec1sierra@gmail.com>
2026-04-17 16:18:45 +0200

    waybar colors and configuraiton improves, wofi wp selector created, matugen configurated

---
+++ dots/.config/hypr/scripts/wp-selector.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Directorio de tus fondos de pantalla
+DIR="$HOME/Pictures/Wallpapers"
+
+if ! pgrep -x "awww-daemon" > /dev/null; then
+    awww-daemon &
+    sleep 0.5
+fi
+
+# Listar archivos, quitando la ruta completa para que wofi solo muestre el nombre
+SELECCION=$(ls "$DIR" | wofi --dmenu --prompt "Selecciona un fondo..." --width 400 --height 300)
+
+# Si seleccionaste algo, aplicarlo
+if [ -n "$SELECCION" ]; then
+    awww img "$DIR/$SELECCION" --transition-type center --transition-step 100 --transition-fps 60
+fi
+
+#EJECUTAR MATUGEN
+matugen image "$DIR/$SELECCION" --source-color-index 0
+
+# 4. RECARGAR COMPONENTES (Opcional pero recomendado)
+# Algunos programas necesitan una señal para leer los nuevos colores
+killall -SIGUSR2 waybar # Recarga Waybar sin cerrarlo
+# hyprctl reload       # Si usas colores de matugen en bordes de Hyprland
---
+++ dots/.config/wofi/config.h
@@ -0,0 +1,6 @@
+mode=drun
+width=500
+height=400
+always_parse_args=true
+show_all=false
+allow_images=true
