afc6df6f182f86be9b4a8c8117d20bd541dc0f3e
TecS1 <tec1sierra@gmail.com>
2026-04-18 11:05:04 +0200

    hyprland windows colored with matugen colors, wofi styles improve and more

--- dots/.config/hypr/scripts/wp-selector.sh
+++ dots/.config/hypr/scripts/wp-selector.sh
@@ -9,17 +9,16 @@ if ! pgrep -x "awww-daemon" > /dev/null; then
 fi
 
 # Listar archivos, quitando la ruta completa para que wofi solo muestre el nombre
-SELECCION=$(ls "$DIR" | wofi --dmenu --prompt "Selecciona un fondo..." --width 400 --height 300)
+SELECCION=$(ls "$DIR" | wofi --dmenu --prompt "Selecciona un fondo..." -c ~/.config/wofi/config.h)
 
 # Si seleccionaste algo, aplicarlo
 if [ -n "$SELECCION" ]; then
     awww img "$DIR/$SELECCION" --transition-type center --transition-step 100 --transition-fps 60
+    #EJECUTAR MATUGEN
+    matugen image "$DIR/$SELECCION" --source-color-index 0
+
+    # 4. RECARGAR COMPONENTES (Opcional pero recomendado)
+    # Algunos programas necesitan una señal para leer los nuevos colores
+    killall -SIGUSR2 waybar # Recarga Waybar sin cerrarlo
+    # hyprctl reload       # Si usas colores de matugen en bordes de Hyprland
 fi
-
-#EJECUTAR MATUGEN
-matugen image "$DIR/$SELECCION" --source-color-index 0
-
-# 4. RECARGAR COMPONENTES (Opcional pero recomendado)
-# Algunos programas necesitan una señal para leer los nuevos colores
-killall -SIGUSR2 waybar # Recarga Waybar sin cerrarlo
-# hyprctl reload       # Si usas colores de matugen en bordes de Hyprland
--- dots/.config/wofi/config.h
+++ dots/.config/wofi/config.h
@@ -1,6 +1,5 @@
-mode=drun
-width=500
-height=400
+width=400
 always_parse_args=true
 show_all=false
 allow_images=true
+image_size=40
